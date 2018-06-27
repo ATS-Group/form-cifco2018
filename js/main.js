335fc74c085efee380f34076334e8fd6705a6b5b
ferjoCode <fernandomenjivar2011@gmail.com>
2018-06-27 15:47:58 -0600

    version 1.0 - frontend sin js

---
+++ js/main.js
@@ -0,0 +1,22 @@
+/** Hacks para que el input file funcione */
+
+var inputs = document.querySelectorAll( '.file-c' );
+Array.prototype.forEach.call( inputs, function( input )
+{
+	var label	 = input.nextElementSibling,
+		labelVal = label.innerHTML;
+
+	input.addEventListener( 'change', function( e )
+	{
+		var fileName = '';
+		if( this.files && this.files.length > 1 )
+			fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+		else
+			fileName = e.target.value.split( '\\' ).pop();
+
+		if( fileName )
+			label.querySelector( 'span' ).innerHTML = fileName;
+		else
+			label.innerHTML = labelVal;
+	});
+});
