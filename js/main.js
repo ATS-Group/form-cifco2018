ebcc5ccb743daf882b210f880eb67071cc3eedc7
FerjoCode <fernandomenjivar2011@gmail.com>
2018-06-28 21:04:08 -0600

    Estilos a comboBox, arreglo subir archivo

--- js/main.js
+++ js/main.js
@@ -9,14 +9,59 @@ Array.prototype.forEach.call( inputs, function( input )
 	input.addEventListener( 'change', function( e )
 	{
 		var fileName = '';
-		if( this.files && this.files.length > 1 )
-			fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
-		else
+
 			fileName = e.target.value.split( '\\' ).pop();
 
-		if( fileName )
+		if( fileName ){
 			label.querySelector( 'span' ).innerHTML = fileName;
-		else
+		}else{
 			label.innerHTML = labelVal;
+		}
 	});
+});
+
+/**Que los estilos del select funcionen */
+
+$('.sel').each(function() {
+	$(this).children('select').css('display', 'none');
+
+	var $current = $(this);
+
+	$(this).find('option').each(function(i) {
+		if(i == 0){
+			$current.prepend($('<div>', {
+				class: $current.attr('class').replace(/sel/g, 'sel__box')
+			}));
+
+			var placeholder = $(this).text();
+			$current.prepend($('<span>', {
+				class: $current.attr('class').replace(/sel/g, 'sel__placeholder'),
+				text: placeholder,
+				'data-placeholder': placeholder
+			}));
+
+			return;
+		}
+
+		$current.children('div').append($('<span>', {
+			class: $current.attr('class').replace(/sel/g, 'sel__box__option'),
+			text: $(this).text()
+		}));
+	});
+});
+
+$('.sel').click(function(){
+	$(this).toggleClass('active');
+});
+
+$('.sel__box__option').click(function(){
+	var txt = $(this).text();
+	var index = $(this).index();
+
+	$(this).siblings('.sel__box__option').removeClass('selected');
+	$(this).addClass('selected');
+
+	var $currentSel = $(this).closest('.sel');
+	$currentSel.children('.sel__placeholder').text(txt);
+	$currentSel.children('select').prop('selectedIndex', index + 1);
 });
